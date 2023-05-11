66412045f39b4bf7a46dce12fc6cae082ed3e9c0
Olivier Gomez <ashitaaaka@gmail.com>
2023-05-11 23:22:26 +0200

    adding search bar function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect} from 'react'
 import './App.css'
 import axios, { Axios } from 'axios'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
+
+//importing components
+import SearchBar from './components/SearchBar'
+
 
 const accessToken = import.meta.env.VITE_API_TOKEN
 
@@ -11,29 +12,32 @@ function App() {
 
     const [location, setLocation] = useState([])
     const [load, setLoad] = useState(false)
+    const [selectedCountry, setSelectedCountry] = useState("");
+    const [selectedCity, setSelectedCity] = useState("");
 
     // Fetch the city to get Location
     useEffect(()=>{
+      
+      if(selectedCity !== "" && selectedCountry !== ""){
       axios
-      .get(
-        `https://api.predicthq.com/v1/places/?q=Lyon,France`,
-        {
-          headers: {
-            'Authorization': accessToken,
-          },
-        }
-      )
-      .then(res => res.data)
-      .then(data => {
-        console.log(data.results)
-        setLocation(data.results[0].location);
-        setLoad(true);
-      })
-      .catch((err)=>{
-        console.log(err)
-      })
-        
-    }, [])
+        .get(
+          `https://api.predicthq.com/v1/places/?q=${selectedCity},${selectedCountry}`,
+          {
+            headers: {
+              'Authorization': accessToken,
+            },
+          }
+        )
+        .then(res => res.data)
+        .then(data => {
+          setLocation(data.results[0].location);
+          setLoad(true);
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+      } 
+    }, [selectedCity])
 
     console.log(location);
 
@@ -42,7 +46,7 @@ function App() {
     if(load) {
       axios
       .get(
-        `https://api.predicthq.com/v1/events/?within=2mi@${location[1]},${location[0]}`,
+        `https://api.predicthq.com/v1/events/?within=1mi@${location[1]},${location[0]}`,
         {
           headers: {
             'Authorization': accessToken,
@@ -57,9 +61,22 @@ function App() {
     }  
   }, [location])
 
-  return (
-      <div>
+  console.log(location);
 
+  return (
+      <div className='home-page'>
+        <div className="container">
+          <div className='title'>
+            <h1 className='home-title'>Undefined</h1>
+            <h2 className='home-subtitle'>Travel</h2>
+          </div>
+          <SearchBar 
+          selectedCountry={selectedCountry}
+          setSelectedCountry={setSelectedCountry}
+          selectedCity={selectedCity}
+          setSelectedCity={setSelectedCity}
+          />
+        </div>
       </div>
   )
 }
